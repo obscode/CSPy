218078c06f04e023324397fbedb57aa26e77fad9
Chris Burns <cburns@carnegiescience.edu>
2020-01-31 09:16:06 -0800

    new scripts

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
+from glob import glob
+scripts = glob('bin/*')
 
 setup(
       name='CSPlib',
@@ -6,7 +8,7 @@ setup(
       author='Chris Burns',
       author_email='cburns@carnegiescience.edu',
       packages=['CSPlib'],
-      scripts=['bin/make_optls_table','bin/make_nirls_table'],
+      scripts=scripts,
       package_data={'CSPlib':['data/*']},
       description='CSP-related python code and scripts',
       requires=[
