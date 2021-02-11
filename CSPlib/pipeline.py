51661ae422c53cf6167af2132bbb4a5c5529b0ce
obscode <cburns@carnegiescience.edu>
2021-02-11 10:56:21 -0800

    check for size plus a integer multiple of 2880

--- CSPlib/pipeline.py
+++ CSPlib/pipeline.py
@@ -52,7 +52,7 @@ class Pipeline:
          prefix/suffix (str):  Prefix  and suffix for raw data. 
                          glob(prefix+'*'+suffix) should return all files.
          fsize (int):  Expected size of the CCD file. If it's not this size
-                       we skip (it could still be reading out)
+                       (plus n*2880), we skip (it could still be reading out)
          calibrations (str): location where older calibration files are located
          templates (str): location where templates are stored
          catalogs (str): location where catalog files are stored
@@ -66,7 +66,7 @@ class Pipeline:
       self.datadir = datadir
       self.prefix = prefix
       self.suffix = suffix
-      self.fsize = fsize
+      self.fsize = [fsize+i*2880 for i in range(3)]  # that should be enough!
       self.update_db = update_db
 
       # A list of all files we've dealt with so far
@@ -208,7 +208,7 @@ class Pipeline:
       flist.sort()
 
       new = [f for f in flist if not os.path.islink(f)]
-      new = [f for f in new if os.path.getsize(f) == self.fsize]
+      new = [f for f in new if os.path.getsize(f) in self.fsize]
       new = [f for f in new if f not in self.rawfiles+self.badfiles]
 
       return new
@@ -586,9 +586,13 @@ class Pipeline:
          # 30 is used internall in photometry code as arbitrary zero-point
          zp = np.sum(diffs*wts)/np.sum(wts) + 30
          ezp = np.sqrt(1.0/np.sum(wts))
-         self.log('Determined zero-point to be {} +/- {}'.format(
-            zp,ezp))
+         if np.isnan(zp) or np.isnan(ezp):
+            self.log("Determining zero-point for frame {} failed (NAN), "\
+                  "skipping...".format(fil))
+            self.ignore.append(fil)
+            continue
 
+         self.log('Determined zero-point to be {} +/- {}'.format(zp,ezp))
          fts = fits.open(fil, memmap=False)
          fts[0].header['ZP'] = zp
          fts[0].header['EZP'] = ezp
@@ -707,11 +711,6 @@ class Pipeline:
             self.log('Template subtraction failed for {}, skipping'.format(
                 fil))
             self.ignore.append(fil)
-
-   def update_db(self):
-      '''For all images that are fully reduced, calibrated, and template-
-      subtracted, update photometry of the transient to the database.'''
-      return
 
    def initialize(self):
       '''Make a first run through the data and see if we have what we need
