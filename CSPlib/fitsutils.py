70c4e80758a70d93dfb750f38a64477f5487038d
Chris Burns <cburns@carnegiescience.edu>
2023-03-11 10:26:13 -0800

    Add extra headers to qdump

--- CSPlib/fitsutils.py
+++ CSPlib/fitsutils.py
@@ -2,13 +2,15 @@
 simple CCD frames (one PrimaryHDU in and HDUList)'''
 
 from astropy.io import fits
+import warnings
 
-def qdump(filename, data, header=None):
+def qdump(filename, data, header=None, extras=None):
    '''quickly produce a FITS file with data and header from another FITS fits.
    Args:
       filename(str):  Name of output file
       data(float arr): data to dump
       header(str or header): Header to use or file with fits header
+      extras(dict):  Extra key-word/value to add to FITS file
    Returns:
       None.
    Effetcs:
@@ -17,11 +19,17 @@ def qdump(filename, data, header=None):
 
    if header is None:
       header = fits.PrimaryHDU(data)
+      if extras is not None:
+         for key in extras:
+            header[0].header[key] = extras[key]
       header.writeto(filename, overwrite=True)
    else:
       if type(header) is str:
          hfts = fits.open(header)
          header = hfts[0].header
+      if extras is not None:
+         for key in extras:
+            header[key] = extras[key]
       fits.writeto(filename, data, header, overwrite=True)
 
 def copyFits(inp):
