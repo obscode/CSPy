03731f72dedf1178dd8159ac7ea2b5da659b4460
Chris Burns <cburns@carnegiescience.edu>
2023-11-28 14:40:25 -0800

    Added remote refcat2 access

--- CSPlib/getRefcat2.py
+++ CSPlib/getRefcat2.py
@@ -18,6 +18,26 @@ elif 'REFCATDIR' in os.environ:
 else:
    refcatdir = '/Volumes/ExtData1/RefCat2'
 
+use_local = False
+if os.path.isdir(refcatdir):
+   if os.path.isdir(os.path.join(refcatdir, '16_m_17')):
+      # Okay, looks like we got it
+      use_local = True
+   
+if not use_local:
+   # refcatdir not found. Can use do remote queries?
+   MASTuser = getattr(cfg.remote, 'MASTuser', None)
+   MASTpasswd = getattr(cfg.remote, 'MASTpasswd', None)
+   if MASTuser is not None and MASTpasswd is not None:
+      try:
+         from mastcasjobs import MastCasJobs
+         jobs = MastCasJobs(username=MASTuser, password=MASTpasswd, 
+                            context="HLSP_ATLAS_REFCAT2")
+      except:
+         jobs = None
+   else:
+      jobs = None
+
 
 def getStarCat(ra, dec, radius, mmin=-10, mmax=100):
    '''Get a list of refcat2 stars plus their photometry.
@@ -30,37 +50,58 @@ def getStarCat(ra, dec, radius, mmin=-10, mmax=100):
       mmax (float): maximum magnitude to return
 
    Returns:
-      astropy.table with catalog data
+      astropy.table with catalog data or None if something went wrong
    '''
+
 
    if not mmin < mmax:
       raise ValueError("mmin must be less than mmax")
    ra = float(ra)*1
    dec = float(dec)*1
 
-   tabs = []
-   if mmin < 16:
-      print(ra,dec)
-      tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
-         [os.path.join(refcatdir, '00_m_16')], 'rc2', verbose=0))
-   if mmin < 17 and mmax > 16:
-      tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
-         [os.path.join(refcatdir, '16_m_17')], 'rc2', verbose=0))
-   if mmin < 178 and mmax > 17:
-      tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
-         [os.path.join(refcatdir, '17_m_18')], 'rc2', verbose=0))
-   if mmax > 18:
-      tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
-         [os.path.join(refcatdir, '18_m_19')], 'rc2', verbose=0))
-   tab = table.vstack(tabs)
-   gids = np.greater_equal(tab['r'], mmin)*np.less_equal(tab['r'], mmax)
-   tab = tab[gids]
+   if use_local:
+      tabs = []
+      if mmin < 16:
+         print(ra,dec)
+         tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
+            [os.path.join(refcatdir, '00_m_16')], 'rc2', verbose=0))
+      if mmin < 17 and mmax > 16:
+         tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
+            [os.path.join(refcatdir, '16_m_17')], 'rc2', verbose=0))
+      if mmin < 178 and mmax > 17:
+         tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
+            [os.path.join(refcatdir, '17_m_18')], 'rc2', verbose=0))
+      if mmax > 18:
+         tabs.append(refcat.RefcatQuery(ra, dec, 1, radius, radius, mmax, 0,
+            [os.path.join(refcatdir, '18_m_19')], 'rc2', verbose=0))
+      tab = table.vstack(tabs)
+      gids = np.greater_equal(tab['r'], mmin)*np.less_equal(tab['r'], mmax)
+      tab = tab[gids]
+ 
+      tab['objID'] = np.arange(1, len(tab)+1)
+      tab = tab['objID','RA','Dec','g','dg','r','dr','i','di']
+      tab.rename_column('Dec','DEC')
+      for filt in ['g','r','i']:
+         tab.rename_column(filt, filt+'mag')
+         tab.rename_column('d'+filt, filt+'err')
+      return tab
+   elif jobs is not None:
+      query = "select r.objid,r.RA,r.Dec,r.g,r.dg,r.r,r.dr,r.i,r.di "\
+              "from refcat2 as r, dbo.fGetNearbyObjEq({},{},{}) as n "\
+              "where r.objid=n.objid".format(ra,dec,radius)
+      tab = jobs.quick(query, task_name='my task')
+      gids = np.greater_equal(tab['r'], mmin)*np.less_equal(tab['r'], mmax)
+      tab = tab[gids]
+      
+      tab['objID'] = np.arange(1, len(tab)+1)
+      tab = tab['objID','RA','Dec','g','dg','r','dr','i','di']
+      tab.rename_column('Dec','DEC')
+      for filt in ['g','r','i']:
+         tab.rename_column(filt, filt+'mag')
+         tab.rename_column('d'+filt, filt+'err')
+      #gids = np.greater_equal(tab['r'], mmin)*np.less_equal(tab['r'], mmax)
+      return tab
 
-   tab['objID'] = np.arange(1, len(tab)+1)
-   tab = tab['objID','RA','Dec','g','dg','r','dr','i','di']
-   tab.rename_column('Dec','DEC')
-   for filt in ['g','r','i']:
-      tab.rename_column(filt, filt+'mag')
-      tab.rename_column('d'+filt, filt+'err')
-   return tab
-
+   else:
+      print("Error:  No local refcat catalog found and remote access failed")
+      return None
