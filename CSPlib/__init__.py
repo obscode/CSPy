ed2f976be4971dd30f563e93f03a095964aa9d93
Chris Burns <cburns@carnegiescience.edu>
2025-11-10 13:51:59 -0800

    Added a more useful version

--- CSPlib/__init__.py
+++ CSPlib/__init__.py
@@ -2,6 +2,6 @@
 CSP data. It is the backbone of the new rapid CSP data reduction pipeline
 to be installed at the Swope, but there's lots of good stuff here.
 '''
-version='0.1.0'
+version='20251109'
 
 from .irafstuff import wairmass_for_lco_images
